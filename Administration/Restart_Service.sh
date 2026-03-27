0c67163c54d705e1cdd4ebff574282085322eacc
Cédric Z <cedriczapart@gmail.com>
2026-03-27 10:23:40 +0100

    Update Script: Restart_Service

--- Administration/Restart_Service.sh
+++ Administration/Restart_Service.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
+#================================================================================================================#
+#                   Description Script Restart_service                                                           #
+# this script allows restart, one or several services                                                            #
+# you must indicated without extension .service example: for mysql.service                                       #
+# you must indicate : mysql                                                                                      #                                                                                                             #
+# script interactif                                                                                                               #       
+#================================================================================================================#
 
-pathdirlog="$HOME/Scripts/Script Linux/Monitoring/Logs"
-pathfilelog="$pathdirlog/Restart_service.log"
+#some command required privileges root
+if [ "$EUID" -ne 0 ];then
+        echo "launch the script with privilege root"
+        exit
+fi
 
+#format date for the logs 
+function Date {
+date '+%Y-%m-%d %H:%M:%S'
+}
+
+# stock one or several service in the variables 
 services_list=()
 
-# pour utiliser la function write-log il faut la syntax suivante:
-# write-log "your message" "your event"
-#example : write-log " the regex found " "INFO"
-
-
+#================================================================================================================#
+#         CONFIGURATION LOGS                                                                                     #
+#================================================================================================================#
+pathdirlog="/var/log/Logs_script_personnal"
+pathfilelog="$pathdirlog/Restart_service.log"
 SEPARATOR="============================================================="
 
 function HeaderLog {
     local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
-    # On utilise echo directement pour écrire les lignes sans le préfixe Date/INFO
+    if [ ! -d "$pathdirlog" ];then
+                mkdir -p "$pathdirlog"
+        fi
+        
     echo "$SEPARATOR" >> "$pathfilelog"
     echo "START SCRIPT: $(basename "$0")" >> "$pathfilelog"
     echo "$SEPARATOR" >> "$pathfilelog"
@@ -23,7 +42,7 @@ function HeaderLog {
     echo "$SEPARATOR" >> "$pathfilelog"
 }
 
-#example : write-log " the regex found " "INFO"
+#example how used write-log : write-log " the regex found " "INFO"
 function write-log {
         local message="$1"
         local event="$2"
@@ -46,19 +65,24 @@ function EndLog {
     echo "$SEPARATOR" >> "$pathfilelog"
 }
 
-
 function check_service {
 
     read -p "Enter the different(s) services, you want managed : " -a services_list
     write-log "List of services :  ${services_list[*]}." "INFO"
 
 	for svc in "${services_list[@]}"; do
+        service_listing=$(systemctl list-unit-files "$svc.service" 2>/dev/null | grep -w "$svc.service" )
+        if [[ -z "$service_listing" ]]; then
+        write-log "service "$svc.service" is not present in listing service" "WARNING"
+        continue
+        fi
+
 		service_active=$(systemctl is-active "$svc" 2>&1) 
 		if [[ "$service_active" == "active" ]]; then
 			write-log "service $svc is running." "INFO"
 		else
             write-log "Service $svc stopped." "INFO"
-			sudo systemctl start "$svc"
+			systemctl start "$svc"
             if [[ $? -eq 0 ]]; then 
             write-log "the $svc has been started. " "INFO"
             else 
@@ -69,7 +93,7 @@ function check_service {
         service_enabled=$(systemctl is-enabled "$svc" 2>&1)
         if [[ "$service_enabled" == "disabled" ]]; then 
             write-log "Service $svc is $service_enabled, next boot the $svc don't running. enabling in progress...." "WARNING"
-            sudo systemctl enable $svc
+            systemctl enable $svc
             if [[ $? -eq 0 ]]; then 
             write-log "the $svc has been enable. " "INFO"
             else 
